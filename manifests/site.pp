095405b0116b562931c8ab0adb27bf5d8017d66c
Marco Süß <shiftmeta@gmail.com>
2016-02-15 16:48:46 +0100

    remove superflous ruby versions

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,10 +64,7 @@ node default {
   nodejs::version { 'v4.2.6': }
 
   # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.8': }
-  ruby::version { '2.2.4': }
+  ruby::version { '2.2.3': }
 
   # common, useful packages
   package {
